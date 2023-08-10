432a2036848d9329a509cda22d04af31efafb0d7
Muhammad <ahmadmuhdpk@gmail.com>
2023-08-11 00:29:35 +0500

    search screen functionality

--- lib/Screens/search_screen.dart
+++ lib/Screens/search_screen.dart
@@ -16,95 +16,104 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   String searchText = '';
   List<Map<String, dynamic>> data = [];
+
+  Future<bool> _onWillPop() async {
+    Navigator.pushReplacementNamed(context, 'home_screen');
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Container(
-          // Add padding around the search bar
-          padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-          // Use a Material design search bar
-          child: TextField(
-            onChanged: (val) {
-              setState(() {
-                searchText = val;
-              });
-            },
-            cursorColor: Colors.black,
-            decoration: kTextFieldDecoration.copyWith(
-              fillColor: const Color(0xffF5F5F5),
-              hintStyle: kText3.copyWith(
-                  fontSize: 15.sp,
-                  color: const Color(0xffbdbdbd),
-                  fontWeight: FontWeight.w400),
-              hintText: 'Search',
-              prefixIcon: Icon(
-                Icons.search_sharp,
-                color: primaryColor,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Container(
+            // Add padding around the search bar
+            padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+            // Use a Material design search bar
+            child: TextField(
+              onChanged: (val) {
+                setState(() {
+                  searchText = val;
+                });
+              },
+              cursorColor: Colors.black,
+              decoration: kTextFieldDecoration.copyWith(
+                fillColor: const Color(0xffF5F5F5),
+                hintStyle: kText3.copyWith(
+                    fontSize: 15.sp,
+                    color: const Color(0xffbdbdbd),
+                    fontWeight: FontWeight.w400),
+                hintText: 'Search',
+                prefixIcon: Icon(
+                  Icons.search_sharp,
+                  color: primaryColor,
+                ),
               ),
             ),
           ),
         ),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: Auth().db.collection('users').snapshots(),
-        builder: (context, snapshots) {
-          return (snapshots.connectionState == ConnectionState.waiting)
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : ListView.builder(
-                  itemCount: snapshots.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    var data = snapshots.data!.docs[index].data()
-                        as Map<String, dynamic>;
-                    if (searchText.isEmpty) {
-                      return Container();
-                    }
-                    if (data['username']
-                        .toString()
-                        .toLowerCase()
-                        .contains(searchText.toLowerCase())) {
-                      return InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MyProfileScreen(
-                                userId: data['userid'],
+        body: StreamBuilder<QuerySnapshot>(
+          stream: Auth().db.collection('users').snapshots(),
+          builder: (context, snapshots) {
+            return (snapshots.connectionState == ConnectionState.waiting)
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: snapshots.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var data = snapshots.data!.docs[index].data()
+                          as Map<String, dynamic>;
+                      if (searchText.isEmpty) {
+                        return Container();
+                      }
+                      if (data['username']
+                          .toString()
+                          .toLowerCase()
+                          .contains(searchText.toLowerCase())) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MyProfileScreen(
+                                  userId: data['userid'],
+                                ),
+                              ),
+                            );
+                          },
+                          child: ListTile(
+                            title: Text(
+                              data['username'],
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: kText2.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            subtitle: Text(
+                              data['name'],
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: kText2,
+                            ),
+                            leading: CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              backgroundImage: NetworkImage(
+                                data['pfp'],
                               ),
                             ),
-                          );
-                        },
-                        child: ListTile(
-                          title: Text(
-                            data['username'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: kText2.copyWith(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
                           ),
-                          subtitle: Text(
-                            data['name'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: kText2,
-                          ),
-                          leading: CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            backgroundImage: NetworkImage(
-                              data['pfp'],
-                            ),
-                          ),
-                        ),
-                      );
-                    }
-                    return Container();
-                  },
-                );
-        },
+                        );
+                      }
+                      return Container();
+                    },
+                  );
+          },
+        ),
       ),
     );
   }
